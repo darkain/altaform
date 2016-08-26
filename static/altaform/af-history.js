ec111ba89071e4ba2a13cbb2f92e11e044d0aa66
DarkainMX <github@darkain.com>
2016-08-25 22:54:43 -0700

    Added history and map JS files

---
+++ static/altaform/af-history.js
@@ -0,0 +1,36 @@
+//Test if html5 history support exists or not
+hasHistory = function() { return !!(window.history && window.history.pushState) };
+
+//AJAX page changing stuffs
+var history_ready = false;
+var history_block = false;
+var history_location = document.location.toString();
+
+
+function afhistory() {
+	History.Adapter.bind(window, 'statechange', function(){
+		if (!hasHistory()) return;
+
+		$('.cpn-profile-sidebar-selected').removeClass('cpn-profile-sidebar-selected');
+
+		var state = History.getState().cleanUrl;
+		console.log(state);
+
+		var start	= state.indexOf('/', 10) + 1;
+		var end		= state.indexOf('/', start);
+		var part	= state.substr(start, end-start);
+
+		if (state != history_location) history_ready = true;
+		if (!history_ready  ||  history_block) return;
+
+		$('#cpn-profile-body')
+			.html('<div class="cpn-loading"></div>')
+			.load(state + '?jq=1', function(response, status, xhr){
+				afSidebar(document.title.split(' - ')[0]);
+				$(this).html( xhr.responseText );
+				$('.cpn-comment-new input').afNewComment();
+			});
+
+		if (typeof ga !== 'undefined') ga('send', 'pageview', location.pathname);
+	});
+}
---
+++ static/altaform/af-map.js
@@ -0,0 +1,95 @@
+//Google Maps
+
+google.maps.visualRefresh = true;
+
+var gmap = 0;
+var gmapInfoWindow = 0;
+var gmapMarkers = new Array();
+var gmapOptions = {
+	zoom: 4,
+	mapTypeId: google.maps.MapTypeId.ROADMAP,
+	center: new google.maps.LatLng(41, -100),
+	styles: [
+		{"stylers":[{"hue":"#00C8E6"}, {"lightness":63}, {"gamma":0.17}]},
+		{"featureType":"water", "stylers":[{"color":"#00C8E6"}]}
+	],
+};
+
+
+
+gmapMarker = function(p1, p2, p3, p4, p5) {
+	if (!gmap) return false;
+
+	if (typeof(p1) === 'object') {
+		var item = {
+			pos: p1,
+			icn: p2,
+			loc: p3,
+			nfo: p4,
+			wnd: false
+		};
+	} else if (typeof(p1) === 'number'  &&  typeof(p2) === 'number') {
+		var item = {
+			pos: new google.maps.LatLng(p1, p2),
+			icn: p3,
+			loc: p4,
+			nfo: p5,
+			wnd: false
+		};
+	} else return false;
+
+	for (i=0; i<gmapMarkers.length; ++i) {
+		if (gmapMarkers[i].pos.toString() == item.pos.toString()) {
+			if (item.nfo) {
+				var content = item.nfo.replace(/<ul(.*?)>/,'');
+				item = gmapMarkers[i];
+				if (item.wnd) {
+					content = item.wnd.getContent().replace('</ul>', content)
+					item.wnd.setContent( content );
+				}
+			}
+			return item;
+		}
+	}
+
+	var image = new google.maps.MarkerImage(
+		item.icn,
+		new google.maps.Size(48, 48),
+		new google.maps.Point(0, 0),
+		new google.maps.Point(24, 48)
+	);	
+
+	item.marker = new google.maps.Marker({
+		map: gmap,
+		position: item.pos,
+		icon: image,
+		anchorPoint: new google.maps.Point(0, -32),
+		shape: {coords:[23,47, 6,15, 14,4, 23,0, 24,0, 33,4, 39,15, 24,47], type:'poly'},
+	});
+
+	if (item.nfo) {
+		item.wnd = new google.maps.InfoWindow({ content: item.loc+item.nfo });
+		google.maps.event.addListener(item.marker, 'click', function() {
+			if (gmapInfoWindow) gmapInfoWindow.close();
+			item.wnd.open(gmap, item.marker);
+			gmapInfoWindow = item.wnd;
+		});
+	}
+
+	gmapMarkers.push(item);
+	return item;
+};
+
+
+
+gmapGeolocate = function(query, url) {
+	service = new google.maps.places.PlacesService(gmap);
+	service.textSearch({'query': query}, function(results, status) {
+		if (status != google.maps.places.PlacesServiceStatus.OK) return;
+
+		var loc = results[0].geometry.location.toString();
+		$.post( url, { 'name':query, 'loc':loc } );
+
+		gmap.setCenter(results[0].geometry.location);
+	});	
+};
