8aad65f3dc484719d6516e76819875972b6b1d56
Vince <github@darkain.com>
2016-08-25 18:18:55 -0700

    Updated for latest Altaform Core

--- _install/install.sql
+++ _install/install.sql
@@ -1,21 +1,26 @@
-CREATE DATABASE IF NOT EXISTS `pudl`;
-USE `pudl`;
-
-
 CREATE TABLE IF NOT EXISTS `pudl_session` (
-	`id` varchar(32) COLLATE ascii_bin NOT NULL COMMENT 'Stores the Session ID',
-	`access` bigint NOT NULL,
-	`address` varchar(127) COLLATE ascii_bin DEFAULT NULL,
-	`data` text COLLATE ascii_bin,
-	PRIMARY KEY (`id`)
+  `id` varchar(64) COLLATE ascii_bin NOT NULL COMMENT 'Stores the Session ID',
+  `user` bigint(20) UNSIGNED NOT NULL DEFAULT '0',
+  `access` bigint(20) UNSIGNED NOT NULL,
+  `address` varchar(127) COLLATE ascii_bin DEFAULT NULL,
+  `data` varbinary(4096) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user` (`user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=ascii COLLATE=ascii_bin;
 
 
 CREATE TABLE IF NOT EXISTS `pudl_user` (
-	`user_id` int unsigned NOT NULL AUTO_INCREMENT,
-	`user_access` set('admin','staff','debug','user','guest') COLLATE ascii_bin NOT NULL DEFAULT 'user',
-	`user_debug` tinyint(1) unsigned NOT NULL DEFAULT '0',
-	`user_ban` tinyint(1) unsigned NOT NULL DEFAULT '0',
-	`user_icon` binary(16) DEFAULT NULL,
-	PRIMARY KEY (`user_id`)
+  `user_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_permission` enum('banned','guest','pending','user','staff','admin','team','deactivated') COLLATE ascii_bin NOT NULL DEFAULT 'user',
+  `user_name` varchar(32) CHARACTER SET utf8 NOT NULL,
+  `user_url` varchar(32) CHARACTER SET ascii DEFAULT NULL,
+  `user_icon` binary(16) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_url` (`user_url`),
+  KEY `user_type` (`user_permission`),
+  KEY `user_icon` (`user_icon`)
 ) ENGINE=InnoDB DEFAULT CHARSET=ascii COLLATE=ascii_bin;
+
+
+ALTER TABLE `pudl_session`
+  ADD CONSTRAINT `pudl_session_ibfk_1` FOREIGN KEY (`user`) REFERENCES `pudl_user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
