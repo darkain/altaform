dba2903a68a625d70d776b8b06e8f298ce707ce2
Vince <github@darkain.com>
2016-11-23 09:47:04 -0800

    AF-REFRESH now calls popdown() first This is useful for single-page systems, such as "sidebar" based pages Whitespace also added to clean things up a bit

--- static/altaform/af-popup.js
+++ static/altaform/af-popup.js
@@ -5,6 +5,7 @@ var popsettings = {
 	minHeight:	350,
 }
 
+
 popup = function(url, title, data) {
 	if (typeof(title)==='object') {
 		title = $('#' + $(title).attr('aria-describedby')).children().html()
@@ -18,9 +19,11 @@ popup = function(url, title, data) {
 	popload(url, data);
 };
 
+
 popdown = function() {
 	$('#popup-window').dialog('close');
 };
+
 
 popload = function(url, data) {
 	$('#popup-window').html('<div class="loading"></div>');
@@ -34,21 +37,25 @@ popload = function(url, data) {
 	});
 }
 
+
 popupdate = function(data) {
 	data = data.trim();
-	if (data == 'AF-OK')		return popdown();
-	if (data == 'AF-REFRESH')	return refresh();
+	if (data == 'AF-OK') return popdown();
+	if (data == 'AF-REFRESH') { popdown(); return refresh() };
 	$('#popup-window').html(data);
 	$('#popup-window .af-default-focus').first().focus();
 };
+
 
 popserial = function() {
 	return $('#popup-window').afSerialize();
 }
 
+
 poppost = function(url, callback) {
 	$.post(url, popserial(), callback?callback:popupdate).fail(poperror);
 }
+
 
 popbuttons = function(buttons, append) {
 	if (append  ||  arguments.length < 2) {
@@ -60,15 +67,18 @@ popbuttons = function(buttons, append) {
 	$('#popup-window').dialog('option', 'buttons', buttons);
 };
 
+
 poptitle = function(title, append) {
 	if (append) title = $('#popup-window').dialog('option', 'title') + ' - ' + title;
 	$('#popup-window').dialog('option', 'title', title);
 };
 
+
 poperror = function(xhr, selector){
 	console.log(xhr);
 	$(selector || '#popup-window').html(xhr.responseText);
 };
+
 
 $(function(){
 	$('#popup-window').dialog($.extend(popsettings, {
