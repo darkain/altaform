802afdee9b6e85c4d59233670625f3675a425906
darkain <git@darkain.com>
2016-12-22 12:36:51 -0800

    Fixing JavaScript compatibility issues with some legacy browsers NOTE: Specifically testing with IE8, but this effects others as well

--- static/altaform/af.js
+++ static/altaform/af.js
@@ -19,25 +19,51 @@ var afSidebar = function(item) {
 	$('.cpn-profile-sidebar-selected').removeClass('cpn-profile-sidebar-selected');
 	$('.cpn-profile-sidebar-item:contains("'+item+'")').addClass('cpn-profile-sidebar-selected');
 	if (typeof afmore === 'function') afmore();
-}
+};
 
 
 //Capitalize first character in a string
-String.prototype.capitalize = function() {
-	return this.charAt(0).toUpperCase() + this.slice(1);
+if (!String.prototype.capitalize) {
+	String.prototype.capitalize = function() {
+		return this.charAt(0).toUpperCase() + this.slice(1);
+	};
 }
 
 
 //Escape a selector
-String.prototype.escape = function() {
-	return this.replace(/(:|\.|\[|\]|\/|\s|,|=)/g, '\\$1');
+if (!String.prototype.escape) {
+	String.prototype.escape = function() {
+		return this.replace(/(:|\.|\[|\]|\/|\s|,|=)/g, '\\$1');
+	};
 }
 
 
 //Strip HTML characters
-String.prototype.striptags = function() {
-	return $(this).text();
-};
+if (!String.prototype.striptags) {
+	String.prototype.striptags = function() {
+		return $(this).text();
+	};
+}
+
+
+//Trim strings http://stackoverflow.com/questions/498970/trim-string-in-javascript
+if (!String.prototype.trim) {
+	String.prototype.trim=function() {
+		return this.replace(/^\s+|\s+$/g, '');
+	};
+
+	String.prototype.ltrim=function(){
+		return this.replace(/^\s+/,'');
+	};
+
+	String.prototype.rtrim=function() {
+		return this.replace(/\s+$/,'');
+	};
+
+	String.prototype.fulltrim=function() {
+		return this.replace(/(?:(?:^|\n)\s+|\s+(?:$|\n))/g,'').replace(/\s+/g,' ');
+	};
+}
 
 
 //Delayed onload
