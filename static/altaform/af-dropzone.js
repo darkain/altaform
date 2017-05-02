c5728dc65dd15f8b0e61f3d0cd6b38ff89b6463b
darkain <git@darkain.com>
2017-05-01 18:19:07 -0700

    Updating Dropzone to be Image/* to fix issues with mobile

--- static/altaform/af-dropzone.js
+++ static/altaform/af-dropzone.js
@@ -13,7 +13,7 @@ var afDropzone = function(form, url, target, maxSize, thumbSize) {
 			clickable:			target,
 			dictDefaultMessage:	'',
 			withCredentials:	true,
-			acceptedFiles:		'image/jpeg,image/png,image/gif',
+			acceptedFiles:		'image/*',
 
 			init: function() {
 				this.on('addedfile', function(file){
