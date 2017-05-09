0e1f6dae4785b5836fd73929f876902841e63a91
darkain <git@darkain.com>
2017-05-08 23:33:41 -0700

    Making afSpreadsheet navigation work on columns of links

--- static/altaform/af-spreadsheet.js
+++ static/altaform/af-spreadsheet.js
@@ -12,30 +12,34 @@
 	};
 
 
-	$.fn.prevRow = function(selector, direction) {
-		var cell	= $(this).closest('td,th');
-		var row		= cell.closest('tr');
+	$.fn.prevRow = function(selector, sel_row, sel_cell, direction) {
+		sel_row		= sel_row	|| 'TR'
+		sel_cell	= sel_cell	|| 'TD,TH';
+
+		var cell	= $(this).is(sel_cell) ? $(this) : $(this).closest(sel_cell);
+		var row		= cell.closest(sel_row);
 		var index	= cell.find(selector).index(this);
-		var item	= row.find('td,th').index(cell);
-		while (row = direction ? row.next('tr') : row.prev('tr')) {
+		var item	= row.find(sel_cell).index(cell);
+
+		while (row = direction ? row.next(sel_row) : row.prev(sel_row)) {
 			if (row.length < 1) break;
-			var thing = row.find('td,th').eq(item).find(selector);
+			var thing = row.find(sel_cell).eq(item).find(selector);
 			if (thing.eq(index).length)	return thing.eq(index);
 			if (thing.last().length)	return thing.last();
 		}
+
 		return $([]);
 	};
 
 
-	$.fn.nextRow = function(selector) {
-		return $(this).prevRow(selector, true);
+	$.fn.nextRow = function(selector, sel_row, sel_cell) {
+		return $(this).prevRow(selector, sel_row, sel_cell, true);
 	};
 
 
 	$.fn.prevInRow = function(selector, row) {
 		selector = selector || $(this).prop('nodeName');
-		row = typeof row !== 'undefined' ? row : 'TR';
-		var set = $(this).closest(row).find(selector);
+		var set = $(this).closest(row||'TR').find(selector);
 		var index = set.index(this);
 		return set.slice(index-1, index);
 	};
@@ -43,16 +47,21 @@
 
 	$.fn.nextInRow = function(selector, row) {
 		selector = selector || $(this).prop('nodeName');
-		row = typeof row !== 'undefined' ? row : 'TR';
-		var set = $(this).closest(row).find(selector);
+		var set = $(this).closest(row||'TR').find(selector);
 		var index = set.index(this);
 		return set.slice(index+1, index+2);
 	};
 
 
 	$.fn.afSpreadsheet = function(options) {
+		//TODO: DONT DOUBLE ADD
+		//TODO: $(this) MAY BE A COLLECTION, NOT A SINGLE ITEM, SO LOOP IT!
+
 		var settings = $.extend({
 			selector:	'select,input,a',
+			row:		'TR',
+			cell:		'TD,TH',
+			column:		false,
 			enter:		true,
 		}, options);
 
@@ -67,25 +76,57 @@
 			//enter
 			if (event.which == 13) {
 				if (settings.enter) {
-					$(this).nextRow(settings.selector).afSelect(event);
+					$(this).nextRow(
+						settings.selector,
+						settings.row,
+						settings.cell
+					).afSelect(event);
 				}
 
 			//up
 			} else if (event.which == 38) {
-				$(this).prevRow(settings.selector).afSelect(event);
+				if (settings.column) {
+					$(this).prevInRow(
+						settings.selector,
+						settings.row
+					).afSelect(event);
+				} else {
+					$(this).prevRow(
+						settings.selector,
+						settings.row,
+						settings.cell
+					).afSelect(event);
+				}
 
 			//down
 			} else if (event.which == 40) {
-				$(this).nextRow(settings.selector).afSelect(event);
+				if (settings.column) {
+					$(this).nextInRow(
+						settings.selector,
+						settings.row
+					).afSelect(event);
+				} else {
+					$(this).nextRow(
+						settings.selector,
+						settings.row,
+						settings.cell
+					).afSelect(event);
+				}
 
 			//left
 			} else if (event.which == 37) {
 				if ($(this).prop('nodeName') == 'INPUT'  &&  ($(this).attr('type')=='text' || $(this).attr('type')=='password')) {
 					if ($(this)[0].selectionStart == 0  &&  $(this)[0].selectionEnd == 0) {
-						$(this).prevInRow(settings.selector).afSelect(event);
+						$(this).prevInRow(
+							settings.selector,
+							settings.row
+						).afSelect(event);
 					}
 				} else {
-					$(this).prevInRow(settings.selector).afSelect(event);
+					$(this).prevInRow(
+						settings.selector,
+						settings.row
+					).afSelect(event);
 				}
 
 			//right
@@ -93,10 +134,16 @@
 				if ($(this).prop('nodeName') == 'INPUT'  &&  ($(this).attr('type')=='text' || $(this).attr('type')=='password')) {
 					var len = $(this).val().length;
 					if ($(this)[0].selectionStart == len  &&  $(this)[0].selectionEnd == len) {
-						$(this).nextInRow(settings.selector).afSelect(event);
+						$(this).nextInRow(
+							settings.selector,
+							settings.row
+						).afSelect(event);
 					}
 				} else {
-					$(this).nextInRow(settings.selector).afSelect(event);
+					$(this).nextInRow(
+						settings.selector,
+						settings.row
+					).afSelect(event);
 				}
 
 			}
